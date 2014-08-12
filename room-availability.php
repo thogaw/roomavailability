a20939c9a751102ed012b4e435745ab4778c9ff6
Thorsten Gawantka <t.gawantka@gmail.com>
2014-08-12 23:10:01 +0200

    Added documentation comment to shortcode root-function.

--- room-availability.php
+++ room-availability.php
@@ -1,45 +1,54 @@
 <?php
 
 /*
-Plugin Name: Room Availability
-Plugin URI: 
-Description: 
-Version: 1.0.0
-Author: Thorsten Gawantka
-Author URI: 
-License: GPLv2
-*/
+  Plugin Name: Room Availability
+  Plugin URI:
+  Description:
+  Version: 1.0.0
+  Author: Thorsten Gawantka
+  Author URI:
+  License: GPLv2
+ */
 
-/* 
-Copyright (C) 2014 Thorsten Gawantka
+/*
+  Copyright (C) 2014 Thorsten Gawantka
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
+/**
+ * Root function of room-availability plugin. This function replaces the
+ * shortcode [roomavailability] or variations of
+ * [roomavailability room="Room 1"] with an overview of all rooms or the
+ * specified room.
+ * 
+ * @param type $atts The shortcodes provided attributes.
+ * @return string The replacement of the shortcode.
+ */
 function render_roomavailability($atts) {
     $a = shortcode_atts(array(
         'room' => NULL
-    ), $atts);
-    
+            ), $atts);
+
     $room = $a['room'];
-    if($room === NULL) {
+    if ($room === NULL) {
         // TODO Render overview
     } else {
         // TODO Render room
     }
-    
+
     return '';
 }
 
