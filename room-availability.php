801db4214a1e15a8adac67da2590911295eebf24
Thorsten Gawantka <t.gawantka@gmail.com>
2014-08-12 23:00:41 +0200

    Created empty shortcode plugin.

---
+++ room-availability.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+Plugin Name: Room Availability
+Plugin URI: 
+Description: 
+Version: 1.0.0
+Author: Thorsten Gawantka
+Author URI: 
+License: GPLv2
+*/
+
+/* 
+Copyright (C) 2014 Thorsten Gawantka
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+function render_roomavailability($atts) {
+    $a = shortcode_atts(array(
+        'room' => NULL
+    ), $atts);
+    
+    $room = $a['room'];
+    if($room === NULL) {
+        // TODO Render overview
+    } else {
+        // TODO Render room
+    }
+    
+    return '';
+}
+
+add_shortcode('roomavailability', 'render_roomavailability');
